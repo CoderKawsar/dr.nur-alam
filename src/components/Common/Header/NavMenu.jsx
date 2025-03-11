ede17cb8fe3387ac0ea01f3261ae8a591fe1a6da
CoderKawsar <coder.kawsar@gmail.com>
2025-03-11 14:00:49 +0600

    about link changed

--- src/components/Common/Header/NavMenu.jsx
+++ src/components/Common/Header/NavMenu.jsx
@@ -106,7 +106,7 @@ export default function NavMenu() {
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link
-              href="/about"
+              href="https://pust.ac.bd/academic/departments/dept_teachers/dept_teachers_profile/100041"
               legacyBehavior
               passHref
               className="font-medium text-base"
